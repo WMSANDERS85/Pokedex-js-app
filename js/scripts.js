dea7fb942efff6423e307f24138be49ba4e9995a
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-05-01 11:19:00 -0700

    Update scripts.js
    Edit Pikachu and Squirtle types. Change from strings to arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList=[
-    {name: "Pikachu", type: "Electric", height: 1.04},
+    {name: "Pikachu", type: ["Electric"], height: 1.04},
     {name: "Charizard", type: ["Fire","Flying"], height: 5.07},
     {name: "Bulbasur", type: ["Grass","Poison"], height: 2.04},
     {name: "Ivysaur", type: ["Grass","Poison"], height: 3.03},
-    {name: "Squritle", type: "Water", height: 1.08},
+    {name: "Squritle", type: ["Water"], height: 1.08},
 
 ]
