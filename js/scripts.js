229ce0f66ba3d3e51692bbbfc553a8cdfe63db08
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-05-09 09:56:02 -0700

    Update scripts.js
    Edit code  syntax for pokemonList and replace for loop with forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 let pokemonList = [
-    { name: "Pikachu", type: ["Electric"], height: 1.04 },
-    { name: "Charizard", type: ["Fire", "Flying"], height: 5.07 },
-    { name: "Bulbasur", type: ["Grass", "Poison"], height: 2.04 },
-    { name: "Ivysaur", type: ["Grass", "Poison"], height: 3.03 },
-    { name: "Squritle", type: ["Water"], height: 1.08 },
+  {
+    name: "Pikachu",
+    type: ["Electric"],
+    height: 1.04
+  },
+  {
+    name: "Charizard",
+    type: ["Fire", "Flying"],
+    height: 5.07
+  },
+  {
+    name: "Bulbasur",
+    type: ["Grass", "Poison"],
+    height: 2.04
+  },
+  {
+    name: "Ivysaur",
+    type: ["Grass", "Poison"],
+    height: 3.03
+  },
+  {
+    name: "Squritle",
+    type: ["Water"],
+    height: 1.08
+  },
 
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-    //Sahil, I commented the line below out because I was not sure if it needed to be there. It was giving me double input.
-    // console.log(pokemonList[i].name + pokemonList[i].height);{
-    if (pokemonList[i].height < 2) {
-        document.write(pokemonList[i].name + " (Height) " + pokemonList[i].height + " This is a small Pokemon" + "<br>");
-    } else if (pokemonList[i].height < 5 && pokemonList[i].height > 2) {
-        document.write(pokemonList[i].name + " (Height) " + pokemonList[i].height + " This is an average Pokemon" + "<br>");
-    } else {
-        document.write(pokemonList[i].name + " (Height) " + pokemonList[i].height + " Wow! That\'s big!" + "<br>");
-    }
-}
-// }
+
+pokemonList.forEach(function (pokemon) {
+  console.log("Name: " + pokemon.name + " Type: " + pokemon.type + " Height " + pokemon.height);
+});
